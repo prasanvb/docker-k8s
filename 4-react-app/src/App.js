6606a13c68f44c694d5a340cd91069660faa7616
prasanv <prasan.venkatb@gmail.com>
2025-07-11 19:29:36 -0700

    feat: add react boiler plate

---
+++ 4-react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Prasan, welcome to react app!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
